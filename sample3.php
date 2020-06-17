2b5b8797213e74cb39d5109a453df2f676bb8798
makiron918 <gonzaress918@gmail.com>
2020-06-17 18:34:18 +0900

    Create sample3

--- sample3.php
+++ sample3.php
@@ -14,17 +14,11 @@
     <h2>Practice</h2>
     <pre>
       <?php
-      $sum = 100+1050+200;
+      $i = 1;
+      print($i . "\n");
+      $i = $i + 1;
+      print($i . "\n");
       ?>
-      <?php
-      $tax = 1.08;
-      ?>
-      合計金額は<?php
-      print($sum);
-      ?>円です
-      税込価格は<?php
-      print($sum*$tax);
-      ?>円です
     </pre>
   </main>
 </body>
