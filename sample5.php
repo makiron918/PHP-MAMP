9bad2cfe806a8ecd145a3f6c80b2b84b3e299490
makiron918 <gonzaress918@gmail.com>
2020-06-19 17:56:09 +0900

    Create sample5.php

---
+++ sample5.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>PHP</title>
+</head>
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+  <main>
+    <h2>Practice</h2>
+    <pre>
+      <?php
+      $week_name = ['日', '月', '火', '水', '木', '金', '土'];
+      print(date('w'));
+      ?>
+    </pre>
+  </main>
+</body>
+</html>
