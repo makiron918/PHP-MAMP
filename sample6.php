2de0e9b1e50691a8d6effd39bd85d12629db8205
makiron918 <gonzaress918@gmail.com>
2020-06-19 18:08:02 +0900

    Create sample6.php

---
+++ sample6.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>PHP</title>
+</head>
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+  <main>
+    <h2>Practice</h2>
+    <pre>
+      <?php
+      $fruits = [
+            'apple'=>'りんご', 
+            'grape'=>'ぶどう', 
+            'lemon'=>'レモン',
+            'tomato'=>'トマト',
+            'peach'=>'もも'];
+      foreach ($fruits as $va) {
+        print($va . "\n");
+      }
+      ?>
+    </pre>
+  </main>
+</body>
+</html>
