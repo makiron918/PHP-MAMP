52695a62ea72f5b10895f31fe8d5105108b8212c
makiron918 <gonzaress918@gmail.com>
2020-06-19 18:14:50 +0900

    Create sample7.php

---
+++ sample7.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>PHP</title>
+</head>
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+  <main>
+    <h2>Practice</h2>
+    <pre>
+      <?php
+      if (date('G') < 11) {
+        print('※ 現在は受付時間外です');
+      }
+      ?>
+    </pre>
+  </main>
+</body>
+</html>
