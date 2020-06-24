7e5345475e037f485a04941d48b4d6a577b35100
makiron918 <gonzaress918@gmail.com>
2020-06-24 21:48:05 +0900

    Create sample8.php

---
+++ sample8.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>PHP</title>
+</head>
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+  <main>
+    <h2>Practice</h2>
+    <pre>
+    <?php
+    $file = file_get_contents('https://h2o-space.com/feed/json/');
+    $json = json_decode($file);
+    ?>
+    </pre>
+  </main>
+</body>
+</html>
