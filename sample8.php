befe7a67f1f28ef21bb97cadb5c78d91c768fa79
makiron918 <gonzaress918@gmail.com>
2020-06-24 21:50:41 +0900

    Update sample8.php

--- sample8.php
+++ sample8.php
@@ -16,7 +16,10 @@
     <?php
     $file = file_get_contents('https://h2o-space.com/feed/json/');
     $json = json_decode($file);
+
+    foreach ($json->items as $item):
     ?>
+    <a href="<?php print($item->url); ?>"><?php print($item->title); ?></a>
     </pre>
   </main>
 </body>
