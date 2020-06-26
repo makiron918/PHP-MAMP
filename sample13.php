7f601da799320e32eae373c50701762bf07430a7
makiron918 <gonzaress918@gmail.com>
2020-06-26 22:01:59 +0900

    Update sample13.php

--- sample13.php
+++ sample13.php
@@ -17,7 +17,7 @@
     $zip = '987-6543';
 
     $zip = mb_convert_kana($zip, 'a', 'UTF-8');
-    if (preg_match("/\A\d{3}[-]\d{4}\z", $zip)) {
+    if (preg_match("/\A\d{3}[-]\d{4}\z/", $zip)) {
       print('郵便番号：〒' . $zip);
     } else {
       print('※ 郵便番号を 123-4567の形式でご記入ください');
