f9cb92f15698c34f2ab18e276625c9a17935c02b
makiron918 <gonzaress918@gmail.com>
2020-06-29 16:04:06 +0900

    Create sample15.php

---
+++ sample15.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>PHP</title>
+</head>
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+  <main>
+    <h2>Practice</h2>
+    <pre>
+      <?php
+      $week = array('金', '土', '日', '月', '火', '水', '木');
+      ?>
+    </pre>
+  </main>
+</body>
+</html>
