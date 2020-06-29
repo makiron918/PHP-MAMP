59f29f8a0e74bf970187129406d16b079b4e82fe
makiron918 <gonzaress918@gmail.com>
2020-06-29 16:12:43 +0900

    Update sample15.php

--- sample15.php
+++ sample15.php
@@ -13,7 +13,19 @@
   <main>
     <h2>Practice</h2>
     <table>
+      <?php
+      for ($i=1; $i<=7; $i++){
+        if ($i % 2) {
+          print('<tr style="background-color: #ccc">');
+        } else {
+          print('<tr>');
+        }
+        print('<td>' . $i . '行目</td>');
+      }
       <tr>
+        <td>1行目</td>
+      </tr>
+      <tr style="background-color: #ccc">
         <td>1行目</td>
       </tr>
     </table>
