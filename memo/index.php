3f09169ee86d2a67646fa3e4efe35c70ae44bb9e
makiron918 <gonzaress918@gmail.com>
2020-06-30 21:14:01 +0900

    Update index.php

--- memo/index.php
+++ memo/index.php
@@ -1,1 +1,26 @@
-トップページです
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>PHP</title>
+</head>
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+  <main>
+    <h2>Practice</h2>
+    <pre>
+      <?php
+      try {
+        $db = new PDO('mysql:dbname=mydb;host=127.0.0.1;charaset=utf8', 'root', 'root');
+      } catch(PDOException $e) {
+        echo 'DB接続エラー: ' . $e->getMessage();
+      }
+      ?>
+    </pre>
+  </main>
+</body>
+</html>
