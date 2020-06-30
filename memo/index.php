13fb1ca33d1a453f2acd643d55b2de92166f3d21
makiron918 <gonzaress918@gmail.com>
2020-06-30 21:25:18 +0900

    Update index.php

--- memo/index.php
+++ memo/index.php
@@ -15,7 +15,7 @@
     <pre>
       <?php
       try {
-        $db = new PDO('mysql:host=localhost;dbname=mydb', 'root', 'root');
+        $db = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', 'root');
       } catch(PDOException $e) {
         echo 'DB接続エラー: ' . $e->getMessage();
       }
