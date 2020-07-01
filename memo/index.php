5b3ef6052b703cb658b0162cd58feaac5cd6b330
makiron918 <gonzaress918@gmail.com>
2020-07-01 22:28:59 +0900

    Update index.php

--- memo/index.php
+++ memo/index.php
@@ -20,8 +20,10 @@
         echo 'DB接続エラー: ' . $e->getMessage();
       }
 
-      $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも", price=210');
-      echo $count . '件のデータを挿入しました';
+      $records = $db->query('SELECT * FROM my_items');
+      while ($record = $records->fetch()) {
+        print($record['item_name'] . "\n");
+      }
       ?>
     </pre>
   </main>
