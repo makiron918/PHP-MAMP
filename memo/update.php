2cbd64863c0dc88f0dc8dd587f52e118cd31bc21
makiron918 <gonzaress918@gmail.com>
2020-07-02 16:54:20 +0900

    Create update.php

---
+++ memo/update.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/style.css">
+  <title>PHP</title>
+</head>
+<body>
+  <header>
+    <h1 class="font-weight-normal">PHP</h1>
+  </header>
+  <main>
+    <h2>Practice</h2>
+
+    <form action="update_do.php" method="post">
+      <textarea name="memo" cols="50" rows="10"></textarea>
+      <button type="submit">登録する</button>
+    </form>
+  </main>
+</body>
+</html>
