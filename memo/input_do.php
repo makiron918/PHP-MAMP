107328ce93e8f4566ccb518bd4bb274c99c840af
makiron918 <gonzaress918@gmail.com>
2020-07-02 16:15:45 +0900

    Update input_do.php

--- memo/input_do.php
+++ memo/input_do.php
@@ -14,14 +14,10 @@
     <h2>Practice</h2>
     <pre>
       <?php
-      try {
-        $db = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', 'root');
-        $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
-        $statement->execute(array($_POST['memo']));
-        echo 'メッセージが登録されました';
-      } catch(PDOException $e) {
-        echo 'DB接続エラー： ' . $e->getMessage();
-      }
+      require('dbconnect.php');
+      $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+      $statement->execute(array($_POST['memo']));
+      echo 'メッセージが登録されました';
       ?>
     </pre>
   </main>
