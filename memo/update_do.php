ce72cb02d8ddaea40f0deb556f35ec7a32a0cb54
makiron918 <gonzaress918@gmail.com>
2020-07-02 21:19:08 +0900

    Update update_do.php

--- memo/update_do.php
+++ memo/update_do.php
@@ -15,7 +15,7 @@
     <pre>
       <?php
       require('dbconnect.php');
-      $statement = $db->prepare('UPDATE memos SET memo=?, WHERE id=?');
+      $statement = $db->prepare('UPDATE memos SET memo=? WHERE id=?');
       $statement->execute(array($_POST['memo'], $_POST['id']));
       ?>
     </pre>
