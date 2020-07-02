9879f0b9642e932ff281f0a97dc3dd95aad1b9cb
makiron918 <gonzaress918@gmail.com>
2020-07-02 16:54:40 +0900

    Update update.php

--- memo/update.php
+++ memo/update.php
@@ -14,7 +14,7 @@
     <h2>Practice</h2>
 
     <form action="update_do.php" method="post">
-      <textarea name="memo" cols="50" rows="10"></textarea>
+      <textarea name="memo" cols="50" rows="10"></textarea><br>
       <button type="submit">登録する</button>
     </form>
   </main>
