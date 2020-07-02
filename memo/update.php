7065e97e7665267486031dd23aaaf46b89536eb1
makiron918 <gonzaress918@gmail.com>
2020-07-02 17:00:47 +0900

    Update update.php

--- memo/update.php
+++ memo/update.php
@@ -1,3 +1,4 @@
+<?php require('dbconnect.php'); ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -13,8 +14,18 @@
   <main>
     <h2>Practice</h2>
 
+    <?php 
+    if (isset($_REQUEST['id']) && is_numeric($_REQUEST['id'])) {
+      $id = $_REQUEST['id'];
+      $memos = $db->prepare('SELECT * FROM memos WHERE id=?');
+      $memos->execute(array($id));
+      $memo = $memos->fetch();
+    }
+    
+    ?>
+
     <form action="update_do.php" method="post">
-      <textarea name="memo" cols="50" rows="10"></textarea><br>
+      <textarea name="memo" cols="50" rows="10"><?php print($memo['memo']); ?></textarea><br>
       <button type="submit">登録する</button>
     </form>
   </main>
